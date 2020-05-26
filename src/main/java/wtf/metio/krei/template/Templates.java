bda28dedb5138c41c07dd845f4ef2248fd028e8f
Sebastian Hoß <seb@ho.de>
2020-05-27 01:07:21 +0200

    fix tests

--- src/main/java/wtf/metio/krei/template/Templates.java
+++ src/main/java/wtf/metio/krei/template/Templates.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
+import static java.lang.System.lineSeparator;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.Collectors.joining;
 
 public final class Templates {
 
@@ -19,7 +21,7 @@ public final class Templates {
         final var template = String.format("/template/%s/%s", category, file);
         try (final var resource = Templates.class.getResourceAsStream(template);
              final var reader = new BufferedReader(new InputStreamReader(resource, UTF_8))) {
-            final var content = reader.lines().collect(Collectors.joining());
+            final var content = reader.lines().collect(joining(lineSeparator(), "", lineSeparator()));
             LOGGER.trace("read [/template{}/{}]\n{}", category, file, content);
             return content;
         } catch (final IOException exception) {

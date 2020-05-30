c2b3b4f1517ff329ebaacf6ba422b326fdc35ec6
Sebastian Hoß <seb@ho.de>
2020-05-30 17:35:24 +0200

    add empty handlers for all high level types

---
+++ src/main/java/wtf/metio/krei/materialize/IDEHandler.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of krei. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of krei,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+package wtf.metio.krei.materialize;
+
+import wtf.metio.krei.model.Unit;
+import wtf.metio.krei.model.ide.IDE;
+
+import java.nio.file.Path;
+import java.util.function.Function;
+
+public final class IDEHandler implements Function<IDE, Unit> {
+
+    private final Path projectDirectory;
+
+    IDEHandler(final Path projectDirectory) {
+        this.projectDirectory = projectDirectory;
+    }
+
+    @Override
+    public Unit apply(final IDE ide) {
+        return Unit.builder().build();
+    }
+
+}
--- src/main/java/wtf/metio/krei/materialize/MaterializeOptions.java
+++ src/main/java/wtf/metio/krei/materialize/MaterializeOptions.java
@@ -8,8 +8,6 @@
 package wtf.metio.krei.materialize;
 
 import org.immutables.value.Value;
-import wtf.metio.krei.model.Project;
-import wtf.metio.krei.model.ProjectConfig;
 import wtf.metio.krei.model.Unit;
 import wtf.metio.krei.model.build.BuildSystem;
 import wtf.metio.krei.model.community.Community;
@@ -20,7 +18,6 @@ import wtf.metio.krei.model.vcs.VCS;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Value.Immutable
 public interface MaterializeOptions {
@@ -32,7 +29,11 @@ public interface MaterializeOptions {
 
     static MaterializeOptions standard(final Path projectDirectory) {
         return builder(projectDirectory)
+                .addBuildHandler(new BuildSystemHandler(projectDirectory))
+                .addCommunityHandler(new CommunityHandler(projectDirectory))
+                .addIdeHandler(new IDEHandler(projectDirectory))
                 .addLicenseHandler(new LicenseHandler(projectDirectory))
+                .addVcsHandler(new VCSHandler(projectDirectory))
                 .build();
     }
     //endregion
@@ -41,11 +42,11 @@ public interface MaterializeOptions {
 
     List<Function<BuildSystem, Unit>> buildHandler();
 
-    List<Function<License, Unit>> licenseHandler();
-
     List<Function<Community, Unit>> communityHandler();
 
     List<Function<IDE, Unit>> ideHandler();
+
+    List<Function<License, Unit>> licenseHandler();
 
     List<Function<VCS, Unit>> vcsHandler();
 

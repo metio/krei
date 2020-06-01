a053c727fbcb2627b66dd066886c44020cf80063
Sebastian Hoß <sebhoss@pm.me>
2020-06-01 16:29:59 +0200

    add javadoc

--- krei-metio/src/main/java/wtf/metio/krei/metio/shared/MetioMavenProject.java
+++ krei-metio/src/main/java/wtf/metio/krei/metio/shared/MetioMavenProject.java
@@ -13,6 +13,13 @@ import java.util.List;
 
 import static wtf.metio.krei.template.GitIgnoreTemplates.MAVEN;
 
+/**
+ * Defines a standard layouts for Maven project at https://metio.wtf/
+ *
+ * @see <a href="https://metio.wtf/">https://metio.wtf/</a>
+ * @see <a href="https://github.com/metio">https://github.com/metio</a>
+ * @see <a href="https://maven.apache.org/">https://maven.apache.org/</a>
+ */
 public final class MetioMavenProject {
 
     public static ProjectConfig configure(final String projectName) {
